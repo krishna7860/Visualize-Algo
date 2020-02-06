b03e2252fc71847b38d0f088406f86c44e213e6b
Coder-Hex <rohansharma.8574@gmail.com>
2020-02-06 09:57:35 +0530

    Bubble Sort

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default class App extends Component {
     return (
       <div className="App">
         <Navbar></Navbar>
-        <Route path="/" component={Home}></Route>
+        <Route path="/" exact component={Home}></Route>
         <Route path="/sorting" component={Sorting}></Route>
       </div>
     );
